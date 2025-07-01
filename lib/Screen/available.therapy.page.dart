618e7468ade62483d7de453bfa9f605e3b538d08
sajivddin <sajivddin.websolutions@gmail.com>
2025-07-01 12:41:11 +0530

    change

--- lib/Screen/available.therapy.page.dart
+++ lib/Screen/available.therapy.page.dart
@@ -78,126 +78,142 @@ class _AvailableTherapyPageState extends ConsumerState<AvailableTherapyPage> {
                       ),
                       child: Column(
                         children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                width: 70.w,
-                                height: 70.h,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.r),
-                                  color: Colors.blueGrey,
+                          InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                CupertinoPageRoute(
+                                  builder: (context) => DoctorInfoPage(),
                                 ),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadiusGeometry.circular(
-                                    10.r,
+                              );
+                            },
+                            child: Row(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Container(
+                                  width: 70.w,
+                                  height: 70.h,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10.r),
+                                    color: Colors.blueGrey,
                                   ),
-                                  child: Image.network(
-                                    // "assets/dot.png",
-                                    availabletherapy
-                                        .users![index]
-                                        .profilePicture
-                                        .toString(),
-                                    fit: BoxFit.cover,
-                                    errorBuilder: (context, error, stackTrace) {
-                                      return Image.network(
-                                        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.png", // fallback if image fails
-                                        fit: BoxFit.cover,
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ),
-                              SizedBox(width: 16.w),
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    // "Dr. Aaron ",
-                                    availabletherapy.users![index].name
-                                        .toString(),
-                                    style: GoogleFonts.inter(
-                                      fontSize: 18.sp,
-                                      fontWeight: FontWeight.w600,
-                                      color: Color(0xFF2B2B2B),
-                                      letterSpacing: -1,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadiusGeometry.circular(
+                                      10.r,
+                                    ),
+                                    child: Image.network(
+                                      // "assets/dot.png",
+                                      availabletherapy
+                                          .users![index]
+                                          .profilePicture
+                                          .toString(),
+                                      fit: BoxFit.cover,
+                                      errorBuilder: (
+                                        context,
+                                        error,
+                                        stackTrace,
+                                      ) {
+                                        return Image.network(
+                                          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.png", // fallback if image fails
+                                          fit: BoxFit.cover,
+                                        );
+                                      },
                                     ),
                                   ),
-                                  Text(
-                                    // "Certified Therapist ",
-                                    availabletherapy
-                                            .users![index]
-                                            .specialization ??
-                                        "null",
-                                    style: GoogleFonts.inter(
-                                      fontSize: 14.sp,
-                                      fontWeight: FontWeight.w500,
-                                      color: Color(0xFF868686),
-                                      letterSpacing: -1,
+                                ),
+                                SizedBox(width: 16.w),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      // "Dr. Aaron ",
+                                      availabletherapy.users![index].name
+                                          .toString(),
+                                      style: GoogleFonts.inter(
+                                        fontSize: 18.sp,
+                                        fontWeight: FontWeight.w600,
+                                        color: Color(0xFF2B2B2B),
+                                        letterSpacing: -1,
+                                      ),
                                     ),
-                                  ),
-                                  Row(
-                                    children: [
-                                      Text(
-                                        availabletherapy.users![index].rating ??
-                                            "0",
-                                        style: GoogleFonts.openSans(
-                                          fontSize: 15.sp,
-                                          fontWeight: FontWeight.w600,
-                                          color: Color(0xFF2B2B2B),
+                                    Text(
+                                      // "Certified Therapist ",
+                                      availabletherapy
+                                              .users![index]
+                                              .specialization ??
+                                          "null",
+                                      style: GoogleFonts.inter(
+                                        fontSize: 14.sp,
+                                        fontWeight: FontWeight.w500,
+                                        color: Color(0xFF868686),
+                                        letterSpacing: -1,
+                                      ),
+                                    ),
+                                    Row(
+                                      children: [
+                                        Text(
+                                          availabletherapy
+                                                  .users![index]
+                                                  .rating ??
+                                              "0",
+                                          style: GoogleFonts.openSans(
+                                            fontSize: 15.sp,
+                                            fontWeight: FontWeight.w600,
+                                            color: Color(0xFF2B2B2B),
+                                          ),
                                         ),
-                                      ),
-                                      SizedBox(width: 4.w),
-                                      RatingBarIndicator(
-                                        rating:
-                                            // double.tryParse(
-                                            //   availabletherapy
-                                            //       .users[index]
-                                            //       .rating,
-                                            // ) ??
-                                            // 0.0,
-                                            double.tryParse(
-                                              availabletherapy
-                                                      .users![index]
-                                                      .rating ??
-                                                  "0",
-                                            ) ??
-                                            0.0,
-                                        itemCount: 5,
-                                        itemSize: 16.sp,
-                                        direction: Axis.horizontal,
-                                        itemBuilder: (context, index) {
-                                          return Icon(
-                                            Icons.star,
-                                            color: buttonColor,
-                                            size: 15.sp,
-                                          );
-                                        },
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                              Spacer(),
-                              GestureDetector(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    CupertinoPageRoute(
-                                      builder: (context) => DoctorInfoPage(),
+                                        SizedBox(width: 4.w),
+                                        RatingBarIndicator(
+                                          rating:
+                                              // double.tryParse(
+                                              //   availabletherapy
+                                              //       .users[index]
+                                              //       .rating,
+                                              // ) ??
+                                              // 0.0,
+                                              double.tryParse(
+                                                availabletherapy
+                                                        .users![index]
+                                                        .rating ??
+                                                    "0",
+                                              ) ??
+                                              0.0,
+                                          itemCount: 5,
+                                          itemSize: 16.sp,
+                                          direction: Axis.horizontal,
+                                          itemBuilder: (context, index) {
+                                            return Icon(
+                                              Icons.star,
+                                              color: buttonColor,
+                                              size: 15.sp,
+                                            );
+                                          },
+                                        ),
+                                      ],
                                     ),
-                                  );
-                                },
-                                child: Text(
-                                  "View Profile ",
-                                  style: GoogleFonts.inter(
-                                    fontSize: 12.sp,
-                                    fontWeight: FontWeight.w500,
-                                    color: buttonColor,
+                                  ],
+                                ),
+                                Spacer(),
+                                GestureDetector(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      CupertinoPageRoute(
+                                        builder: (context) => DoctorInfoPage(),
+                                      ),
+                                    );
+                                  },
+                                  child: Text(
+                                    "View Profile ",
+                                    style: GoogleFonts.inter(
+                                      fontSize: 12.sp,
+                                      fontWeight: FontWeight.w500,
+                                      color: buttonColor,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                           SizedBox(height: 15.h),
                           Row(
