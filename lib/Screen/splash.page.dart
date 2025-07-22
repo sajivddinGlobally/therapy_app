d9166729fe740bd7c4d32682454f4245809ebb5e
sajivddin <sajivddin.websolutions@gmail.com>
2025-07-22 17:08:19 +0530

    change

--- lib/Screen/splash.page.dart
+++ lib/Screen/splash.page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:therapy_app/Screen/doctorHome.page.dart';
-import 'package:therapy_app/Screen/doctorList.page.dart';
+
 import 'package:therapy_app/Screen/login.page.dart';
 import 'package:therapy_app/constant/myColor.dart';
 
