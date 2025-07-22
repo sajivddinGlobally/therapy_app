8981d11dcc7f2c152b8abd8ad9757d391cb242c2
sajivddin <sajivddin.websolutions@gmail.com>
2025-07-22 15:07:07 +0530

    change

--- lib/Screen/doctorProfilePage.dart
+++ lib/Screen/doctorProfilePage.dart
@@ -1,5 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:therapy_app/constant/myColor.dart';
 
 class DoctorProfilePage extends StatefulWidget {
   const DoctorProfilePage({super.key});
@@ -9,8 +16,145 @@ class DoctorProfilePage extends StatefulWidget {
 }
 
 class _DoctorProfilePageState extends State<DoctorProfilePage> {
+  File? image;
+  final picker = ImagePicker();
+
+  Future pickImageFromGallery() async {
+    var status = await Permission.camera.request();
+    if (status.isGranted) {
+      final PickedFile = await picker.pickImage(source: ImageSource.gallery);
+      if (PickedFile != null) {
+        setState(() {
+          image = File(PickedFile.path);
+        });
+      }
+    }
+    print("Camera Permission isDenied");
+  }
+
+  Future pickImageFromCamera() async {
+    var status = await Permission.camera.request();
+    if (status.isGranted) {
+      final PickedFile = await picker.pickImage(source: ImageSource.camera);
+      if (PickedFile != null) {
+        setState(() {
+          image = File(PickedFile.path);
+        });
+      }
+    }
+    print("Gallery Permission isDenied");
+  }
+
+  Future showImage() async {
+    showCupertinoModalPopup(
+      context: context,
+      builder:
+          (context) => CupertinoActionSheet(
+            actions: [
+              CupertinoActionSheetAction(
+                onPressed: () {
+                  pickImageFromCamera();
+                  Navigator.pop(context);
+                },
+                child: Text("Camera"),
+              ),
+
+              CupertinoActionSheetAction(
+                onPressed: () {
+                  pickImageFromGallery();
+                  Navigator.pop(context);
+                },
+                child: Text("Gallery"),
+              ),
+            ],
+          ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: bgColor,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 30.h),
+          Center(
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      width: 150.w,
+                      height: 150.h,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xFFF5F5F7),
+                      ),
+                      child:
+                          image == null
+                              ? ClipOval(
+                                child: Image.asset(
+                                  "assets/frame.png",
+                                  // "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1665px-No-Image-Placeholder.png",
+                                  fit: BoxFit.cover,
+                                ),
+                              )
+                              : ClipOval(
+                                child: Image.file(image!, fit: BoxFit.cover),
+                              ),
+                    ),
+                    // Positioned(
+                    //   right: 10.w,
+                    //   bottom: 10.h,
+                    //   child: GestureDetector(
+                    //     onTap: () {
+                    //       showImage();
+                    //     },
+                    //     child: Container(
+                    //       width: 30.w,
+                    //       height: 30.h,
+                    //       decoration: BoxDecoration(
+                    //         borderRadius: BorderRadius.circular(10.r),
+                    //         color: buttonColor,
+                    //       ),
+                    //       child: Center(
+                    //         child: Icon(
+                    //           Icons.edit,
+                    //           color: Colors.white,
+                    //           size: 20.sp,
+                    //         ),
+                    //       ),
+                    //     ),
+                    //   ),
+                    // ),
+                  ],
+                ),
+                SizedBox(height: 14.h),
+                Text(
+                  "Name",
+                  style: GoogleFonts.nunito(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF2B2B2B),
+                    letterSpacing: -1,
+                  ),
+                ),
+                Text(
+                  "Email",
+                  style: GoogleFonts.inter(
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF868686),
+                    letterSpacing: -1,
+                    height: 1.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
