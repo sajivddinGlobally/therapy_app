f5cc2d02397eeee823027fae97a1066ca00a1d43
sajivddin <sajivddin.websolutions@gmail.com>
2025-07-22 14:50:53 +0530

    chnage

--- lib/Screen/doctorList.page.dart
+++ lib/Screen/doctorList.page.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -39,6 +38,7 @@ class _DoctorListPageState extends State<DoctorListPage> {
     return Scaffold(
       backgroundColor: bgColor,
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: bgColor,
         title: Text(
           "Top Doctors",
---
+++ lib/Screen/doctorProfilePage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DoctorProfilePage extends StatefulWidget {
+  const DoctorProfilePage({super.key});
+
+  @override
+  State<DoctorProfilePage> createState() => _DoctorProfilePageState();
+}
+
+class _DoctorProfilePageState extends State<DoctorProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
