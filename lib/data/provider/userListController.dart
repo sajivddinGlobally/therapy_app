29431f2872a55b063c017db046477aea691d2f11
sajivddin <sajivddin.websolutions@gmail.com>
2025-07-25 16:02:58 +0530

    change

--- lib/data/provider/userListController.dart
+++ lib/data/provider/userListController.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:therapy_app/core/network/api.state.dart';
 import 'package:therapy_app/core/utils/pretty.dio.dart';
@@ -7,3 +9,30 @@ final userListController = FutureProvider<UserListModel>((ref) async {
   final userListSerivce = ApiStateNetwork(createDio());
   return await userListSerivce.fetchUserList();
 });
+
+
+///  state Notifier se service get api ki
+
+
+// class UserListNotifier extends StateNotifier<AsyncValue<UserListModel>> {
+//   UserListNotifier() : super(AsyncValue.loading()) {
+//     fetchUserList();
+//   }
+
+//   Future<void> fetchUserList() async {
+//     try {
+//       final userService = ApiStateNetwork(createDio());
+//       final service = await userService.fetchUserList();
+//       state = AsyncData(service);
+//       log(service.bookings.toString());
+//     } catch (e, s) {
+//       state = AsyncError(e, s);
+//       log(e.toString());
+//     }
+//   }
+// }
+
+// final userListProvider =
+//     StateNotifierProvider<UserListNotifier, AsyncValue<UserListModel>>(
+//       (ref) => UserListNotifier(),
+//     );
